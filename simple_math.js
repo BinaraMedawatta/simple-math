074d0cc8089de765b321f6a9477628cc845e4ad0
Binara Medawatta <binaramedawatta@gmail.com>
2020-12-14 09:07:38 +0530

    bug fixes

--- simple_math.js
+++ simple_math.js
@@ -2,7 +2,7 @@ export function isOdd(x) { return (x%2==0) ?  false : true}
 
 
 export function isPrime(x) {
-    for(i=2; i<=x/2; i++) if(x%i==0) return false
+    for(var i=2; i<=x/2; i++) if(x%i==0) return false
     return true
 }
 
@@ -13,14 +13,14 @@ export function isDivisibleByX(n, x) { return (n%x==0) ? true : false}
 export function factorial(x) {
     if(x<0) return 0
     var n = 1
-    for(i=x; i>0; i--) n *= i
+    for(var i=x; i>0; i--) n *= i
     return n
 }
 
 
 export function primesLessThanX(x) {
     var primes = []
-    for(j=2; j<x; j++){
+    for(var j=2; j<x; j++){
         if(isPrime(j)) primes.push(j)
     }
     return primes
@@ -32,7 +32,7 @@ export function primesBetweenXandY(x, y, i_x = false, i_y = false) {
     var primes = []
     first = i_x ? x : x + 1 
     last = i_y ? y + 1 : y
-    for(j=first; j<last; j++){
+    for(var j=first; j<last; j++){
         if(isPrime(j)) primes.push(j)
     }
     return primes
@@ -42,6 +42,7 @@ export function primesBetweenXandY(x, y, i_x = false, i_y = false) {
 export function primeFactorsOfX(x) {
     if(isPrime(x)) return x
     var out = []
+    var i
     var primes = primesLessThanX(x)
     for(i in primes){
         if(isDivisibleByX(x, primes[i])) out.push(primes[i])
